062508da402f98707b3cc9931cb4f2610681c80c
Matheus Levy <srlevy008@gmail.com>
2023-05-05 11:02:46 -0300

    pequenas auterações
    fiz uma class controladorControle e configurei os getmapping e post mapping

---
+++ exemplo01/src/main/java/br/com/relacionamento/relacionamentoentidade/controle/ControladorControle.java
@@ -0,0 +1,27 @@
+package br.com.relacionamento.relacionamentoentidade.controle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.relacionamento.relacionamentoentidade.modelo.Colaborador;
+import br.com.relacionamento.relacionamentoentidade.repositorio.ColaboradorRepositorio;
+
+@RestController
+@RequestMapping("/colaboradores")
+public class ControladorControle {
+    @Autowired
+    private ColaboradorRepositorio colaborador_repositorio;
+
+    @GetMapping("/listar")
+    public Iterable<Colaborador> listar(){
+        return colaborador_repositorio.findAll();
+    }
+    @PostMapping("/cadastrar")
+    public Colaborador cadastrar(@RequestBody Colaborador obj){
+        return colaborador_repositorio.save(obj);
+    }
+}
